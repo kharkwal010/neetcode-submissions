d885cd44e15b8e1dbf2dedcd217a36b3e9ce6c4d
kharkwal010 <kharkwalankit010@gmail.com>
2026-07-02 01:04:05 +0530

    Time: 59 ms (77.38%) | Memory: 74.9 MB (87.51%) - LeetSync

--- 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.cpp
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.cpp
@@ -6,26 +6,19 @@ public:
         sort(events.begin(), events.end());
         int i = 0;
         int count = 0;
-        while(i<events.size()){
+        while(i<events.size() || !minheap.empty()){
+            if(minheap.empty()) time = events[i][0];
             while(i<events.size() && time>=events[i][0]){
                 minheap.push(events[i][1]);
                 i++;
             }
-            while(!minheap.empty() && time>minheap.top()) minheap.pop();
+            while(!minheap.empty() && minheap.top()<time) minheap.pop();
             if(!minheap.empty()){
                 minheap.pop();
+                time++;
                 count++;
             }
-            time++;
         }
-        while(!minheap.empty()){
-            while(!minheap.empty() && time>minheap.top()) minheap.pop();
-            if(!minheap.empty()){
-                minheap.pop();
-                count++;
-            }
-            time++;
-        }
-        return count;
+        return count;           
     }
 };
