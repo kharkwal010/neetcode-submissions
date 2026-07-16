44da7560dd79d6e9b7c43e2e9f106da786e259a8
kharkwal010 <kharkwalankit010@gmail.com>
2026-07-17 00:44:30 +0530

    Time: 3 ms (12.42%) | Memory: 14.4 MB (6.64%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
-        vector<int> ans = prices;
         stack<pair<int, int>> st;
         for(int i=0; i<prices.size(); i++){
-                while(!st.empty() && st.top().first>=prices[i]){
-                    ans[st.top().second] = st.top().first - prices[i];
-                    st.pop();
-                }
-                st.push({prices[i], i});
+            while(!st.empty() && st.top().first>=prices[i]){
+                prices[st.top().second] = st.top().first - prices[i];
+                st.pop();
+            }
+            st.push({prices[i], i});
         }
-        return ans;
+        return prices;
     }
 };
