a1d980ddc5e738349139e5c9fa621df6a7918554
kharkwal010 <kharkwalankit010@gmail.com>
2026-07-23 21:50:51 +0530

    Time: 58 ms (95.99%) | Memory: 36.7 MB (87.81%) - LeetSync

--- 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.cpp
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
     vector<int> smallestRange(vector<vector<int>>& nums) {
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> minheap;
-        // priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> mindel;
-        int maxi = INT_MIN;
-        for(int i=0; i<nums.size(); i++){
-            minheap.push({nums[i][0], i, 0});
-            maxi = max(maxi, nums[i][0]);
+       vector<int> index(nums.size(), 0);
+       int curr = INT_MAX;
+       vector<int> ans;
+       priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minheap;
+       int maxi = INT_MIN;
+       for(int i=0; i<nums.size(); i++){
+        minheap.push({nums[i][0], i});
+        maxi = max(maxi, nums[i][0]);
+       }
+       while(true){
+        auto top = minheap.top();
+        minheap.pop();
+        int mini = top.first;
+        int imin = top.second;        
+        // cout<<maxi<<" "<<mini<<endl;
+        if(curr>maxi-mini){
+            curr = maxi - mini;
+            ans = {mini, maxi};
         }
-        int interval = maxi - minheap.top()[0];
-        vector<int> ans = {minheap.top()[0], maxi};
-        while(true){
-            auto top = minheap.top();
-            minheap.pop();
-            int r = top[1];
-            int c = top[2];
-            if(c==nums[r].size()-1) break;
-            minheap.push({nums[r][c+1], r, c+1});
-            maxi = max(maxi, nums[r][c+1]);
-            if(maxi-minheap.top()[0]<interval){
-                interval = maxi - minheap.top()[0];
-                ans = {minheap.top()[0], maxi};
-                if(interval==0) break;
-            }            
-        }
-        return ans;
+        index[imin]++;
+        int j = index[imin];
+        if(j==nums[imin].size()) break;      
+        maxi = max(maxi, nums[imin][j]);
+        minheap.push({nums[imin][j], imin});        
+       }
+       return ans;
     }
 };
