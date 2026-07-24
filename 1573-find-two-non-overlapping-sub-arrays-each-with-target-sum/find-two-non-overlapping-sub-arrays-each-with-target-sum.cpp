65698fe8a80a8ee5ed6f835f7f29722b0c30390d
kharkwal010 <kharkwalankit010@gmail.com>
2026-07-25 00:45:48 +0530

    Time: 327 ms (12.77%) | Memory: 178.8 MB (8.16%) - LeetSync

--- 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
+++ 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
@@ -1,38 +1,34 @@
 class Solution {
 public:
     int minSumOfLengths(vector<int>& arr, int target) {
-        vector<int> prefix(arr.size(), INT_MAX);
-        vector<int> suffix(arr.size(), INT_MAX);
+        int sum = 0;
+        int n = arr.size();
         unordered_map<int, int> index;
         index[0] = -1;
-        int mini = INT_MAX;
-        int sum = 0;
+        vector<int> left(arr.size(), INT_MAX);
+        vector<int> right(arr.size(), INT_MAX);
+        int curr = INT_MAX;
         for(int i=0; i<arr.size(); i++){
+            left[i] = curr;
             sum += arr[i];
-            if(index.count(sum - target)){
-                mini = min(mini, i - index[sum - target]);
-            }
-            prefix[i] = mini;
+            if(index.count(sum-target)) curr = min(curr, i - index[sum-target]);
             index[sum] = i;
         }
-        index.clear();
-        index[0] = arr.size();
+        curr = INT_MAX;
         sum = 0;
-        mini = INT_MAX;
-        for(int i=arr.size()-1; i>=0; i--){
-            sum+=arr[i];
-            if(index.count(sum - target)){
-                mini = min(mini, index[sum - target] - i);
-            }
-            suffix[i] = mini;
+        index.clear();
+        index[0] = n;
+        for(int i=n-1; i>=0; i--){
+            sum += arr[i];
+            if(index.count(sum-target)) curr = min(curr, index[sum-target] - i);
+            right[i]= curr;
             index[sum] = i;
         }
         long long ans = INT_MAX;
-        for(int i=0; i<arr.size(); i++){
-            long long prev = (i>0) ? prefix[i-1] : INT_MAX;
-            ans = min(ans, prev + suffix[i]);
+        for(int i=0; i<n; i++){
+            ans = min(ans, (long long)left[i] + right[i]);
         }
-        if(ans==INT_MAX) return -1;
-        return ans;
+        return (ans==INT_MAX) ? -1 : ans;
+
     }
 };
