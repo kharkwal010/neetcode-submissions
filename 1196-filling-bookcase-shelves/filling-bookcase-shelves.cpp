d1c070fda4aa86c4ec2311623e36aacf9fbe97a8
kharkwal010 <kharkwalankit010@gmail.com>
2026-07-24 15:35:46 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (42.02%) - LeetSync

--- 1196-filling-bookcase-shelves/filling-bookcase-shelves.cpp
+++ 1196-filling-bookcase-shelves/filling-bookcase-shelves.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    unordered_map<int, int> memo;
-    int height(vector<vector<int>>& books, int width, int i){
+    vector<int> memo;
+    int height(vector<vector<int>>& books, int& width, int i){
         if(i==books.size()) return 0;
-        if(memo.count(i)) return memo[i];
-        int high = 0;
+        if(memo[i]!=-1) return memo[i];
         int wide = 0;
         int ans = INT_MAX;
+        int high = 0;
         for(int j=i; j<books.size(); j++){
-            if(width - wide< books[j][0]) break;
             wide += books[j][0];
-            high= max(books[j][1], high);
-            ans = min(ans, high + height(books, width, j+1));
+            high = max(high, books[j][1]);
+            if(wide>width) break;
+            ans = min(ans, high + height(books, width, j+1));            
         }
-        memo[i] = ans;
-        return memo[i];
+        return memo[i] = ans;
     }
     int minHeightShelves(vector<vector<int>>& books, int shelfWidth) {
+        memo.resize(books.size(), -1);
         return height(books, shelfWidth, 0);
+
     }
 };
