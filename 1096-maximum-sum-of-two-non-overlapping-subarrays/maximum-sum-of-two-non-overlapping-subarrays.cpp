0a91367a8d3c1cfb2c7267252fb019698001e4dc
kharkwal010 <kharkwalankit010@gmail.com>
2026-07-25 12:34:38 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (48.81%) - LeetSync

--- 1096-maximum-sum-of-two-non-overlapping-subarrays/maximum-sum-of-two-non-overlapping-subarrays.cpp
+++ 1096-maximum-sum-of-two-non-overlapping-subarrays/maximum-sum-of-two-non-overlapping-subarrays.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     int maxSumTwoNoOverlap(vector<int>& nums, int firstLen, int secondLen) {
-
+        int prev_max = INT_MIN;
         vector<int> prefix(nums.size()+1, 0);
-        for(int i=0; i<nums.size(); i++) prefix[i+1] = prefix[i] + nums[i];
-        // one first;
-        int ans = 0;
-        int maxi= prefix[firstLen] - prefix[0];
-        for(int i= firstLen + secondLen; i<prefix.size(); i++){
-            maxi = max(maxi, prefix[i-secondLen] - prefix[i-secondLen-firstLen]);
-            ans = max(ans, prefix[i] - prefix[i-secondLen] + maxi);
+        for(int i=0; i<nums.size(); i++){
+            prefix[i+1] = prefix[i] + nums[i];
         }
-        // cout<<ans<<endl;
-        int maxi2 = prefix[secondLen] - prefix[0];
-        for(int i= firstLen + secondLen; i<prefix.size(); i++){
-            maxi2 = max(maxi2, prefix[i-firstLen] - prefix[i-secondLen-firstLen]);
-            ans = max(ans, prefix[i] - prefix[i-firstLen] + maxi2);
-        }  
-        // cout<<ans<<endl;
-        return ans;     
-
+        int ans = INT_MIN;
+        for(int i = firstLen + secondLen; i<=nums.size(); i++){
+            prev_max = max(prev_max, prefix[i-secondLen] - prefix[i-firstLen-secondLen]);
+            int curr = prefix[i] - prefix[i-secondLen];
+            ans = max(ans, prev_max + curr);
+        }
+        prev_max = INT_MIN;
+        for(int i = firstLen + secondLen; i<=nums.size(); i++){
+            prev_max = max(prev_max, prefix[i-firstLen] - prefix[i-firstLen-secondLen]);
+            int curr = prefix[i] - prefix[i-firstLen];
+            ans = max(ans, prev_max + curr);
+        }
+        return ans;
     }
 };
