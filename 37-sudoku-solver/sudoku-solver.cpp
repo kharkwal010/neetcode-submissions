4d8ee15b12a95373ac205bf2c0eb55303d655310
kharkwal010 <kharkwalankit010@gmail.com>
2026-08-06 22:39:03 +0530

    Time: 435 ms (21.19%) | Memory: 9.1 MB (10.84%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -2,47 +2,58 @@ class Solution {
 public:
     vector<vector<bool>> row;
     vector<vector<bool>> col;
-    vector<vector<bool>> box;
-
-    bool check(vector<vector<char>>& board){
+    vector<vector<bool>> block;
+    vector<vector<char>> ans;
+    void bfs(vector<vector<char>>& board, int& count){
         for(int i=0; i<9; i++){
             for(int j=0; j<9; j++){
-                if(board[i][j]=='.'){
-                    int b = (j/3) + (i/3) * 3; 
-                    for(int k=1; k<10; k++){
-                        if(row[i][k] || col[j][k] || box[b][k]) continue;
-                        board[i][j] = '0' + k;
-                        row[i][k] = true;
-                        col[j][k] = true;
-                        box[b][k] = true;
-                        if(check(board)) return true;
-                        board[i][j] = '.';
-                        row[i][k] = false;
-                        col[j][k] = false;
-                        box[b][k] = false;                       
+                if(board[i][j] == '.'){
+                    int b = 3*(i/3) + (j/3);
+                    for(int k=1; k<=9; k++){
+                        if(!row[i][k] && !col[j][k] && !block[b][k]){
+                            char ch = k + '0';
+                            board[i][j]  = ch;
+                            row[i][k] = true;
+                            col[j][k] = true;
+                            block[b][k] = true;
+                            count--;
+                            bfs(board, count);
+                            count++;
+                            board[i][j]  = '.';
+                            row[i][k] = false;
+                            col[j][k] = false;
+                            block[b][k] = false;                            
+                        }
                     }
-                    return false;
+                    return;
                 }
             }
         }
-        return true;
-    }
-    void solveSudoku(vector<vector<char>>& board) {
-        row.resize(9, vector<bool>(10, false));
-        col.resize(9, vector<bool>(10, false));
-        box.resize(9, vector<bool>(10, false));
-        for(int i=0; i<9; i++){
-            for(int j=0; j<9; j++){
-                if(board[i][j]!='.'){
-                    int term = board[i][j]-'0';
-                    row[i][term] = true;
-                    col[j][term] = true;
-                    int b = (j/3) + (i/3) * 3;
-                    box[b][term] = true;
-                }
-            }
+        if(count==0){
+            ans = board;
         }
-        check(board);
         return;
+    }
+
+    void solveSudoku(vector<vector<char>>& board) {
+       row.resize(9, vector<bool>(10, false));
+       col.resize(9, vector<bool>(10, false));
+       block.resize(9, vector<bool>(10, false));
+       int count = 0;
+       for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                int b = 3*(i/3) + (j/3);
+                if(board[i][j]!='.'){
+                    int num = board[i][j]-'0';
+                    row[i][num] = true;
+                    col[j][num] = true;
+                    block[b][num] = true;
+                }
+                else count++;
+            }
+       }
+       bfs(board, count);
+       board = ans;
+       return;
     }
 };
