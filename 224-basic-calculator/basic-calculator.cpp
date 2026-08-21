722d9ba43bcc932618bd2285d68f9fa42d63b883
kharkwal010 <kharkwalankit010@gmail.com>
2026-08-21 16:35:20 +0530

    Time: 23 ms (5.76%) | Memory: 17 MB (7.51%) - LeetSync

--- 224-basic-calculator/basic-calculator.cpp
+++ 224-basic-calculator/basic-calculator.cpp
@@ -1,38 +1,55 @@
 class Solution {
 public:
     int calculate(string s) {
+        // here i am going to use (--> INT_MAX; and -(--> INT_MIN;
+        long long curr = 0;
+        bool add = true;
         stack<long long> st;
-        long long term = 0;
-        int sign = 1;
-        int i=0;
-        stack<int> op;
-        while(i<s.size()){
-            if(!(s[i]-'0'<=9 && s[i]-'0'>=0)){
-                if(s[i]=='+') sign = 1;
-                else if(s[i]=='-') sign = -1;
-                else if(s[i]=='('){
-                    st.push(term);
-                    term = 0;
-                    op.push(sign);
-                    sign = 1;
-                }
-                else if(s[i]==')'){
-                    long long t = st.top();
-                    st.pop();
-                    term = term*op.top()+t;
-                    op.pop();
-                }
-                i++;
-            }
-            else{
-                long long t = 0;
-                while(i<s.size() && (s[i]-'0'<=9 && s[i]-'0'>=0)){
-                    t = 10*t + s[i]-'0';
+        for(int i=0; i<s.size(); i++){
+            if(s[i]-'0'>=0 && s[i]-'0'<=9){
+                int l=i;
+                while(i<s.size() && s[i]-'0'>=0 && s[i]-'0'<=9){
                     i++;
                 }
-                term += sign*t;
+                long long t = stoll(s.substr(l, i-l));
+                if(add) st.push(t);
+                else st.push(-1*t);
+                i--;
             }
+            else{
+                if(s[i]=='-') add = false;
+                else if(s[i]=='+') add = true;
+                else if(s[i]=='('){
+                    if(add) st.push(LLONG_MAX);
+                    else st.push(LLONG_MIN);
+                    add=true;
+                }
+                
+                if(s[i]==')'){
+                    long long num = 0;
+                    while(!(st.top()==LLONG_MIN || st.top()==LLONG_MAX)){
+                        // cout<<st.top()<<endl;
+                        num += st.top();
+                        st.pop();
+                    }
+                    long long top = st.top();
+                    st.pop();
+                    if(top==LLONG_MAX){
+                        st.push(num);
+                    }
+                    else{
+                        st.push(-1*num);
+                    }
+                }
+                curr = 0;
+            }            
         }
-        return term;
+        int ans = 0;
+        while(!st.empty()){
+            cout<<st.top()<<endl;
+            ans += st.top();
+            st.pop();
+        }
+        return ans;
     }
 };
