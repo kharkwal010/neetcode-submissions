ef2ef3abedd860208547baefb6c8df551c256e83
kharkwal010 <kharkwalankit010@gmail.com>
2026-08-21 16:00:29 +0530

    Time: 23 ms (58.22%) | Memory: 15.6 MB (64.02%) - LeetSync

--- 149-max-points-on-a-line/max-points-on-a-line.cpp
+++ 149-max-points-on-a-line/max-points-on-a-line.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
-    int inf = 10e5;
     int maxPoints(vector<vector<int>>& points) {
-        int x = 0;
-        unordered_map<int, unordered_map<int, int>> count;
+        int ans = 1;
         for(int i=0; i<points.size(); i++){
+            map<pair<int, int>, int> mp;
             for(int j=i+1; j<points.size(); j++){
-                int del_y = (points[j][1] - points[i][1]);
-                int del_x = (points[j][0] - points[i][0]);
-                int sign = 1;
-                if(del_y*del_x<0) sign = -1;
-                int div = gcd(del_x, del_y);
+                int del_x = points[i][0] - points[j][0];
+                int del_y = points[i][1] - points[j][1];
+                if(del_x==0){
+                    del_y = 1;                    
+                }
+                else if(del_y==0){
+                    del_x=1;
+                }
+                else{
+                    int neg =((del_x*del_y) < 0) ? -1 : 1;
+                    int div = gcd(del_x, del_y);
+                    del_x = neg * abs(del_x/div);
+                    del_y = abs(del_y/div);
+                }
+                mp[{del_x, del_y}]++;
                 
-                int term = count[sign*abs(del_x/div)][abs(del_y/div)]++; 
-                x = max(x, term+1);               
+                
+                ans=max(ans, mp[{del_x, del_y}]+1);                
             }
-            count.clear();
+            mp.clear();
         }
-        return x+1;
+        return ans;
     }
 };
