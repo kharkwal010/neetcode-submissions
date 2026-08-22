7fa470679ef40080779c64f2b15415833eb2543e
kharkwal010 <kharkwalankit010@gmail.com>
2026-08-22 10:59:12 +0530

    Time: 7 ms (81.32%) | Memory: 51.7 MB (94.43%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,24 +1,25 @@
 class Solution {
-public:
-    unordered_map<int, bool> memo;
-    bool dfs(vector<vector<int>>& graph, int i){
-        if(graph[i].size()==0){
-            return memo[i] = true;
+public:   
+    vector<int> cycle;
+    int dfs(vector<vector<int>>& graph, int i){
+        if(graph[i].empty()) return cycle[i] = 0;
+        if(cycle[i]!=-1) return cycle[i];
+        cycle[i] = INT_MAX;
+        int ans = -1;
+        for(int nei: graph[i]){
+            ans = max(ans, dfs(graph, nei));
         }
-        if(memo.count(i)) return memo[i];
-        memo[i] = false;        // wapis aaye to false krde
-        bool ans = true;
-        for(int nei: graph[i]) ans = ans && dfs(graph, nei);
-        return memo[i] = ans;
+        return cycle[i] = ans;
+
     }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n = graph.size();
-        for(int i=0; i<n; i++){
-            if(!memo.count(i)) dfs(graph, i);
+        cycle.resize(graph.size(), -1);
+        for(int i=0; i<graph.size(); i++){
+            if(cycle[i]==-1) dfs(graph, i);
         }
         vector<int> ans;
-        for(int i=0; i<n; i++){
-            if(memo[i]) ans.push_back(i);
+        for(int i=0; i<cycle.size(); i++){
+            if(cycle[i]==0) ans.push_back(i);
         }
         return ans;
     }
