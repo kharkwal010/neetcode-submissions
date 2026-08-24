e9b513a22841ea829923a376f011936f3951f11c
kharkwal010 <kharkwalankit010@gmail.com>
2026-08-24 11:57:03 +0530

    Time: 36 ms (66.25%) | Memory: 89.3 MB (78.16%) - LeetSync

--- 1831-ways-to-split-array-into-three-subarrays/ways-to-split-array-into-three-subarrays.cpp
+++ 1831-ways-to-split-array-into-three-subarrays/ways-to-split-array-into-three-subarrays.cpp
@@ -1,33 +1,53 @@
 class Solution {
 public:
+    int lower(vector<int>& terms, int l, int r, int limit){
+        int ans = r + 1;
+        while(l<=r){
+            int m = (l+r)/2;
+            if(terms[m]<limit){
+                l = m + 1;
+            }
+            else{
+                // cout<<limit<<" "<<m<<" "<<terms[m]<<endl;
+                ans = m;
+                r = m - 1;
+            }
+        }
+        return ans;
+    }
+    int upper(vector<int>& terms, int l, int r, int limit){
+        int ans = l-1;
+        while(l<=r){
+            int m = (l+r)/2;
+            if(terms[m]<=limit){
+                // cout<<limit<<" "<<m<<" "<<terms[m]<<endl;
+                ans = m;
+                l = m + 1;
+            }
+            else{
+                r = m - 1;
+            }
+        }
+        return ans;
+    }
     int waysToSplit(vector<int>& nums) {
-        vector<long long> prefix;
-        prefix.push_back(nums[0]);
-        for(int i=1; i<nums.size(); i++){
-            prefix.push_back(prefix.back() + nums[i]);
-        }
-        long long sum = prefix[nums.size()-1];
-        int n = prefix.size();
-        int j = 1;
-        int k = 1;
-        int i=0;
+      vector<int> prefix = nums;
+      for(int i=1; i<nums.size(); i++) prefix[i] = prefix[i-1] + prefix[i];
+        int sum = prefix[nums.size()-1];
         int mod = 1e9+7;
+        int l = 0;
+        int r = nums.size() - 2;
         long long ans = 0;
-        while(i<n-2 && prefix[i]<=sum/3){
-            long long tar = sum - prefix[i];
-            long long low = 2*prefix[i];
-            long long high = prefix[i] + tar/2;
-            while(j<n && (j<=i || prefix[j]<low)){
-                j++;
-            }
-            while(k<n-1 && (k<j || prefix[k]<=high)){
-                k++;
-            }
-            ans = (ans + (k - j));
-            i++;
-
+        while(l<nums.size()-2 && prefix[l]<=sum/3){
+            int llimit = prefix[l]*2;
+            int ulimit = prefix[l] + (sum - prefix[l])/2;
+            int lb = lower(prefix, l+1, r, llimit);
+            int ub = upper(prefix, l+1, r, ulimit);
+            int diff = max(0, ub - lb + 1);
+            ans = (ans + diff) % mod;
+            l++;
         }
-        return ans%mod;
+        return ans;
 
     }
 };
