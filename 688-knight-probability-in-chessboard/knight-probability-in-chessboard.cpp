6148706039c92be10848d50066f064ac82f25f70
kharkwal010 <kharkwalankit010@gmail.com>
2026-08-26 00:05:40 +0530

    Time: 7 ms (63.52%) | Memory: 11.5 MB (48.84%) - LeetSync

--- 688-knight-probability-in-chessboard/knight-probability-in-chessboard.cpp
+++ 688-knight-probability-in-chessboard/knight-probability-in-chessboard.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    vector<vector<int>> dir = {{2, 1}, {2, -1}, {-2, 1}, {-2, -1}, {1, 2}, {-1, 2} , {-1, -2}, {1, -2}};
+    vector<vector<int>> dir = {{2,1}, {2,-1}, {-2,1}, {-2,-1}, {1,2}, {1,-2}, {-1,2}, {-1,-2}};
     vector<vector<vector<double>>> memo;
-
-    double prob(int row, int col, int k, int& n){
-        if(k==0) return 1;
-        if(memo[row][col][k]!=-1) return memo[row][col][k];
+    double prob(int n, int k, int r, int c){
+        if(k==0) return 1.0;
+        if(memo[r][c][k]!=-1) return memo[r][c][k];
         double ans = 0;
         for(int i=0; i<8; i++){
-            int nr = row + dir[i][0];
-            int nc = col + dir[i][1];
-            if(nr>=n || nr<0 || nc>=n || nc<0) continue;
-            ans += prob(nr, nc, k-1, n)/8;
-        }
-        return memo[row][col][k] = ans;
+            int nr = r + dir[i][0];
+            int nc = c + dir[i][1];
+            if(nr<0 || nc<0 || nc>=n || nr>=n) continue;
+            ans += 1.0/8*prob(n, k-1, nr, nc);
+        }                
+        return memo[r][c][k] = ans;
     }
     double knightProbability(int n, int k, int row, int column) {
         memo.resize(n, vector<vector<double>>(n, vector<double>(k+1, -1)));
-        return prob(row, column, k, n);
+        return prob(n, k, row, column);
     }
 };
