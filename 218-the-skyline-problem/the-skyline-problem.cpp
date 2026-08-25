ccf69f0fdb54c6a0a5b4eb4278a51fa2cb53c7da
kharkwal010 <kharkwalankit010@gmail.com>
2026-08-25 18:07:52 +0530

    Time: 6 ms (98.45%) | Memory: 25.3 MB (99.57%) - LeetSync

--- 218-the-skyline-problem/the-skyline-problem.cpp
+++ 218-the-skyline-problem/the-skyline-problem.cpp
@@ -1,30 +1,48 @@
 class Solution {
 public:
+    priority_queue<pair<int, int>> minheap;
     vector<vector<int>> getSkyline(vector<vector<int>>& buildings) {
-       priority_queue<pair<int, int>> maxheap;
-       vector<vector<int>> ans;
-       int i = 0;
-       while(i<buildings.size() || !maxheap.empty()){
-            int x;
-            if(maxheap.empty() || (i<buildings.size() && maxheap.top().second>=buildings[i][0])){
-                x = buildings[i][0];
+        sort(buildings.begin(), buildings.end());
+        vector<vector<int>> ans;
+        int i = 0;
+        while(i<buildings.size()){
+            int st = buildings[i][0];
+            int end = buildings[i][1];
+            int height = buildings[i][2];
+            if(minheap.empty()){
+                ans.push_back({st, height});
+                minheap.push({height, end});
+                i++;
+                continue;
             }
-            else{
-                x = maxheap.top().second;
-            }
-            while(i<buildings.size() && buildings[i][0]<=x){
-                maxheap.push({buildings[i][2], buildings[i][1]});
+            int prev = minheap.top().second;
+            int ph = minheap.top().first;
+            if(prev>=st){
+                // cout<<ph<<" "<<height<<endl;
+                if(ph<height){
+                    if(ans.back()[0]==st){
+                        height = max(height, ans.back()[1]);
+                        ans.pop_back();                        
+                    }
+                    ans.push_back({st,height});
+                }
+                minheap.push({height, end});
                 i++;
             }
-            while(!maxheap.empty() && maxheap.top().second<=x){
-                maxheap.pop();
+            else{
+                minheap.pop();
+                while(!minheap.empty() && minheap.top().second<=prev) minheap.pop();
+                int h = (minheap.empty()) ? 0: minheap.top().first;
+                ans.push_back({prev, h});
             }
-            int h = (maxheap.empty()) ? 0 : maxheap.top().first;
-            if(ans.size()==0 || ans.back()[1]!=h){
-                ans.push_back({x, h});
-            }
-            
-       }
-       return ans;
+        }
+        while(!minheap.empty()){
+            int prev = minheap.top().second;
+            minheap.pop();
+            while(!minheap.empty() && minheap.top().second<=prev) minheap.pop();
+            int h = (minheap.empty()) ? 0: minheap.top().first;
+            ans.push_back({prev, h});
+        }
+        return ans;
     }
 };
