48b88fa411374332c70d35fdf5591b81767a0078
kharkwal010 <kharkwalankit010@gmail.com>
2026-08-26 01:54:10 +0530

    Time: 4 ms (54.87%) | Memory: 10.4 MB (48.23%) - LeetSync

--- 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.cpp
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    int check(string& s, int l, int r, int& k){
-        if(r-l+1<k) return 0;
+    int sub(string& s, int&k, int l, int r){
         vector<int> freq(26, 0);
         for(int i=l; i<=r; i++) freq[s[i]-'a']++;
-        int left = l;
         int ans = 0;
+        int left = l;
         for(int i=l; i<=r; i++){
             if(freq[s[i]-'a']<k){
-                ans = max(ans, check(s, left, i-1, k));
+                ans = max(ans, sub(s, k, left, i-1));
                 while(left<=i){
                     freq[s[left]-'a']--;
                     left++;
-                }                
+                }
             }
         }
-        // cout<<r-left+1<<endl;
         return max(ans, r-left+1);
     }
     int longestSubstring(string s, int k) {
-        return check(s, 0, (int)s.size()-1, k);
+      return sub(s, k, 0, s.size()-1);
+        
     }
 };
