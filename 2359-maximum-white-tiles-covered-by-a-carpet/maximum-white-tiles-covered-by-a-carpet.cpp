cbc08a6e2c3cb29f1dea77191b413164b1946f0a
kharkwal010 <kharkwalankit010@gmail.com>
2026-08-28 01:38:18 +0530

    Time: 33 ms (91.21%) | Memory: 74 MB (63.00%) - LeetSync

--- 2359-maximum-white-tiles-covered-by-a-carpet/maximum-white-tiles-covered-by-a-carpet.cpp
+++ 2359-maximum-white-tiles-covered-by-a-carpet/maximum-white-tiles-covered-by-a-carpet.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
     int maximumWhiteTiles(vector<vector<int>>& tiles, int carpetLen) {
-        int n = tiles.size();
         sort(tiles.begin(), tiles.end());
-        
-        queue<pair<int, int>> q;
-        int ans = 0;
+        int ans = 0;        
         int j = 0;
-        int val = 0;
-        int carry = 0;
-        for(int i=0; i<n; i++){
-            int curr = tiles[i][0];
-            int end = curr+carpetLen-1;         
-            while(j<n && tiles[j][1]<=end){
-                q.push({tiles[j][0], tiles[j][1]});
-                val += (tiles[j][1] - tiles[j][0] + 1);
+        int covered = 0;
+        int i = 0;
+        while(j<tiles.size() && i<tiles.size()){
+            int st = tiles[i][0];
+            int end = st + carpetLen - 1;
+            int partial = 0;
+            while(j<tiles.size() && tiles[j][0]<=end){
+                if(tiles[j][1]<=end){
+                    covered += (tiles[j][1] - tiles[j][0] + 1);
+                }
+                else{
+                    partial = (end-tiles[j][0]+1);
+                    break;
+                }
                 j++;
             }
-            int extra = 0;
-            if(j<n && tiles[j][0]<=end){
-                extra = end - tiles[j][0] + 1; 
-            }
-            // cout<<val<<endl;
-            val += extra;
-            ans = max(ans, val);     
-            val -= extra; 
-            val -= (tiles[i][1] - tiles[i][0] + 1);     
-            
+            ans = max(ans, covered + partial);
+            if(i==j) break;
+            covered -= (tiles[i][1] - tiles[i][0] + 1);
+            i++;
         }
         return ans;
+        
     }
 };
