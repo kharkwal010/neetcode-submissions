e9e1509d1bd7e04813ae086b54e2d98f01890a7a
kharkwal010 <kharkwalankit010@gmail.com>
2026-08-27 22:06:40 +0530

    Time: 52 ms (16.24%) | Memory: 106.3 MB (22.17%) - LeetSync

--- 3113-beautiful-towers-ii/beautiful-towers-ii.cpp
+++ 3113-beautiful-towers-ii/beautiful-towers-ii.cpp
@@ -2,57 +2,48 @@ class Solution {
 public:
     long long maximumSumOfHeights(vector<int>& maxHeights) {
         int n = maxHeights.size();
-        vector<int> right(n, 0);
+        vector<long long> right(n, 0);
         stack<pair<int, int>> st;
         for(int i=0; i<n; i++){
             while(!st.empty() && st.top().first>maxHeights[i]){
-                int top = st.top().second;
+                right[st.top().second] = i - st.top().second;
                 st.pop();
-                right[top] = (i - top);
             }
             st.push({maxHeights[i], i});
         }
         while(!st.empty()){
-            int top = st.top().second;
+            right[st.top().second] = n - st.top().second;
             st.pop();
-            right[top] = n - top;
-        }
-        vector<long long> rightsum(n, 0);
-        long long sum = 0;
-        for(int i=n-1; i>=0; i--){
-            long long end = (i + right[i]==n) ? 0 : rightsum[i + right[i]];
-            sum = (long long)maxHeights[i]*right[i] + end;
-            rightsum[i] = sum;            
         }
 
-        vector<int> left(n, 0);
+        for(int i=n-1; i>=0; i--){
+            long long extra = (i+right[i]<n) ? right[i+right[i]] : 0;
+            long long curr = maxHeights[i]*right[i] + extra;
+            right[i] = curr;
+        }
+
+        vector<long long> left(n, 0);
         for(int i=n-1; i>=0; i--){
             while(!st.empty() && st.top().first>maxHeights[i]){
-                int top = st.top().second;
+                left[st.top().second] = st.top().second - i;
                 st.pop();
-                left[top] = (top - i);
             }
             st.push({maxHeights[i], i});
         }
         while(!st.empty()){
-            int top = st.top().second;
+            left[st.top().second] = st.top().second + 1;
             st.pop();
-            left[top] = 1 + top;
         }
-        vector<long long> leftsum(n, 0);
-        sum = 0;
+
         for(int i=0; i<n; i++){
-            long long end = (i - left[i]==-1) ? 0 : leftsum[i - left[i]];
-            sum = (long long)maxHeights[i]*left[i] + end;
-            leftsum[i] = sum;
-            
+            long long extra = (i-left[i]>=0) ? left[i-left[i]] : 0;
+            long long curr = maxHeights[i]*left[i] + extra;
+            left[i] = curr;
         }
         long long ans = 0;
         for(int i=0; i<n; i++){
-            ans = max(ans, leftsum[i] + rightsum[i] - maxHeights[i]);
-            // cout<<maxHeights[i]<<" "<< leftsum[i]<<" "<<rightsum[i]<<endl;
+            ans = max(ans, left[i] + right[i] - maxHeights[i]);
         }
         return ans;
-
     }
 };
