bb5456d3ca732e50791fccf5356ee6b9acd388b9
kharkwal010 <kharkwalankit010@gmail.com>
2026-08-27 09:37:50 +0530

    Time: 24 ms (44.25%) | Memory: 34.5 MB (13.83%) - LeetSync

--- 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.cpp
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.cpp
@@ -1,50 +1,55 @@
 class Solution {
 public:
-    
     vector<int> findSubstring(string s, vector<string>& words) {
-        unordered_map<string, int> temp;
+        unordered_map<string, int> points;
+        for(string& w: words) points[w]++;
+        int number = words.size();
         vector<int> ans;
-        int n = words.size();
-        for(string& w: words){
-            temp[w]++;
-        }
-        int sz = words[0].size();
-        for(int i=0; i<sz; i++){
-            vector<string> subs;
-            for(int j=i; j<s.size(); j+=sz){
-                if(j+sz<=s.size()){
-                    subs.push_back(s.substr(j, sz));
-                }
+
+        int n = words[0].size();
+        for(int i=0; i<n; i++){
+            vector<string> terms;
+            for(int j=i; j+n<=s.size(); j+=n){
+                terms.push_back(s.substr(j, n));
+                // cout<<s.substr(j, n)<<" ";
             }
+            // cout<<endl;
+
             int l = 0;
+            int count = 0;
             int r = 0;
-            int count=0;
-            unordered_map<string, int> terms = temp;
-            while(r<subs.size()){
-                if(terms[subs[r]]>0){
-                    terms[subs[r]]--;
+            
+            while(r<terms.size()){
+                if(!points.count(terms[r])){
+                    while(l<r){
+                        points[terms[l]]++;
+                        l++;
+                    }
+                    count = 0;
+                    r++;
+                    l++;
+                }
+                else{
+                    while(points[terms[r]]==0){
+                        points[terms[l]]++;
+                        l++;
+                        count--;
+                    }  
+                    points[terms[r]]--;
                     count++;
-                    if(count==n){
-                        ans.push_back(i+l*sz);
-                        terms[subs[l]]++;
+                    if(count==number){
+                        ans.push_back(i+n*l);
+                        points[terms[l]]++;
                         count--;
                         l++;
                     }
                     r++;
                 }
-                else{
-                    if(l==r){
-                        l++;
-                        r++;
-                    }
-                    else{
-                        terms[subs[l]]++;
-                        count--;
-                        l++;
-                    }
-                }
             }
-            
+            while(l<r){
+                points[terms[l]]++;
+                l++;
+            }
         }
         return ans;
     }
