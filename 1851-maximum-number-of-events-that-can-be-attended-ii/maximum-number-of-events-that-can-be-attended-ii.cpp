e058188d9d06d5a7347377388d412b921ed631ee
kharkwal010 <kharkwalankit010@gmail.com>
2026-08-28 22:33:54 +0530

    Time: 171 ms (79.80%) | Memory: 168.2 MB (47.33%) - LeetSync

--- 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.cpp
+++ 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> memo;
-    int solve(vector<vector<int>>& events, int k, int i){
+     int value(vector<vector<int>>& events, int k, int i){
         if(i==events.size() || k==0) return 0;
         if(memo[i][k]!=-1) return memo[i][k];
-        int next = events[i][1] + 1;
-        // cout<<i<<" "<<next<<endl;
+        int ans = 0;
+        ans = max(ans, value(events, k, i+1));
         int l = i+1;
         int r = events.size()-1;
-        int ans = r+1;
+        int target = events[i][1] + 1;
+        // smallest number greater/ equal to target
+        int next = r+1;
         while(l<=r){
-            int m = (l+r) / 2;
-            if(events[m][0]>=next){
-                // cout<<"jeal"<<endl;
-                ans = m;
+            int m = (l+r)/2;
+            if(events[m][0]<target) l = m+1;
+            else{
+                next = m;
                 r = m-1;
             }
-            else l = m+1;
         }
-        // cout<<next<<" "<<ans<<endl;
-        int skip = solve(events, k, i+1);
-        int take = events[i][2] + solve(events, k-1, ans);
-        return memo[i][k] = max(take, skip);
+        ans = max(ans, events[i][2] + value(events, k-1, next));
+        return memo[i][k] = ans;
     }
     int maxValue(vector<vector<int>>& events, int k) {
         int n = events.size();
-        memo.resize(n, vector<int>(k+1, -1));
+        memo.resize(n, vector<int>(k+1,-1));
         sort(events.begin(), events.end());
-        return solve(events, k, 0);
+        return value(events, k, 0);        
     }
 };
