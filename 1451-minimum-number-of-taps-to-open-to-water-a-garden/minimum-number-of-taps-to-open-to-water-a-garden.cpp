6c1296c37741ca546a0c435c5526378f0b875974
kharkwal010 <kharkwalankit010@gmail.com>
2026-09-01 14:27:08 +0530

    Time: 3 ms (50.89%) | Memory: 19.3 MB (64.05%) - LeetSync

--- 1451-minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ 1451-minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     int minTaps(int n, vector<int>& ranges) {
-        vector<int> rightmax(n+1, 0);
-        for(int i=0; i<ranges.size(); i++){
-            if(ranges[i]==0) continue;
-            int l = max(0, i - ranges[i]);
-            int r = min(n, i + ranges[i]);
-            rightmax[l] = max(rightmax[l], r);
-        }
-        int far = 0;
-        int maxcurr = 0;
-        int i = 0;
-        int count = 0;
-        while(i<n){
-            while(i<=maxcurr){
-                far = max(far, rightmax[i]);
-                i++;
+       vector<int> jumps(n+1, 0);
+       for(int i=0; i<ranges.size(); i++){
+            int left = max(0, i - ranges[i]);
+            int right = min(n, i + ranges[i]);
+            jumps[left] = max(jumps[left], right); 
+       }
+       int count = 0;
+       int prev_far = 0;
+       int i = 0;
+       while(i<n){
+            int far = prev_far;
+            for(int j=i; j<=prev_far; j++){
+                far = max(far, jumps[j]);
             }
+            if(prev_far==far) return -1;
             count++;
+            i = prev_far+1;
+            prev_far = far;
             if(far==n) return count;
-            else if(far<=maxcurr) return -1;
-            maxcurr = far;
-        }
-        return -1;
 
+       }
+       return count;
+              
     }
 };
