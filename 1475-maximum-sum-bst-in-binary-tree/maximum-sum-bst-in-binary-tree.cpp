9cd97e938bb04026fde42b3e2703a549e0f6921d
kharkwal010 <kharkwalankit010@gmail.com>
2026-09-02 14:26:12 +0530

    Time: 794 ms (6.60%) | Memory: 235.5 MB (6.54%) - LeetSync

--- 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.cpp
+++ 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.cpp
@@ -17,22 +17,29 @@ class Solution {
 public:
     int ans;
     vector<int> binary(TreeNode* root){
-        if(!root) return {INT_MIN, 0, INT_MAX, 1};
-        vector<int> left = binary(root->left);
-        vector<int> right = binary(root->right);
-        int bin = 1;
-        int sum = 0;
-        if(left[3]==1 && right[3]==1){
-            if(left[0]<root->val && right[2]>root->val){
-                sum = root->val + left[1] + right[1];
-                ans = max(ans, sum);
-                int left_max = max({root->val, left[0], right[0]});
-                int right_min = min({root->val, left[2], right[2]});
-                return {left_max, sum, right_min, 1};
-            }
+        if(!root) return {INT_MIN, INT_MAX, 0, true};           //{maxi, mini, sum, is_bst}
+        auto left = binary(root->left);
+        auto right = binary(root->right);
+        int maxi = root->val;
+        int mini = root->val; 
+        int lsum = 0;
+        int rsum = 0;
+        bool one = false;
+        bool two = false;
+        if(left[3] && left[0]<root->val){
+            one = true;
+            lsum = left[2];
+            mini = min(mini, left[1]);
         }
-        return {INT_MAX, 0, INT_MIN, 0};
-        
+        if(right[3] && right[1]>root->val){
+            two = true;
+            rsum = right[2];
+            maxi = max(maxi, right[0]);
+        }
+        if(!(one && two)) return {0, 0, 0, false};
+        int curr = root->val + lsum + rsum;
+        ans = max(ans, curr);
+        return {maxi, mini, curr, true};        
     }
     int maxSumBST(TreeNode* root) {
         ans = 0;
