3f3ba0a95fe242bbda38ca587095e8e3a44f5cae
kharkwal010 <kharkwalankit010@gmail.com>
2026-09-04 13:12:09 +0530

    Time: 64 ms (5.53%) | Memory: 76.8 MB (5.02%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,30 +1,33 @@
 class Solution {
-public:
+public:    
     vector<int> visited;
-    void dfs(vector<vector<int>>& adj, int i){
-        // cout<<i <<" "<<adj[i].size()<<endl;
-        for(int nei: adj[i]){
-            if(visited[nei]) continue;
-            visited[nei] = true;
-            dfs(adj, nei);
+    void bfs(vector<vector<int>>& adj, int curr){
+        queue<int> q;
+        q.push(curr);
+        while(!q.empty()){
+            int top = q.front();
+            q.pop();
+            for(int nei: adj[top]){
+                if(visited[nei]) continue;
+                visited[nei] = true;
+                q.push(nei);
+            }
         }
         return;
     }
     int makeConnected(int n, vector<vector<int>>& connections) {
-        int con = connections.size();
-        if(con<n-1) return -1;
-        vector<vector<int>> adj(n);
-        for(auto ele: connections){
-            adj[ele[0]].push_back(ele[1]);
-            adj[ele[1]].push_back(ele[0]);
-        }
+        if(connections.size()<n-1) return -1;
         visited.resize(n, false);
+        vector<vector<int>> adj(n);
+        for(auto ed: connections){
+            adj[ed[0]].push_back(ed[1]);
+            adj[ed[1]].push_back(ed[0]);
+        }
         int count = 0;
         for(int i=0; i<n; i++){
             if(!visited[i]){
-                // cout<<i<<endl;
                 visited[i] = true;
-                dfs(adj, i);
+                bfs(adj, i);
                 count++;
             }
         }
