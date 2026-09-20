c67e3d5af808c56426b55085c7247c72c55aff8b
kharkwal010 <kharkwalankit010@gmail.com>
2026-09-20 12:57:26 +0530

    Time: 54 ms (21.19%) | Memory: 28.5 MB (17.80%) - LeetSync

--- 4310-count-good-integers-in-a-range/count-good-integers-in-a-range.cpp
+++ 4310-count-good-integers-in-a-range/count-good-integers-in-a-range.cpp
@@ -1,31 +1,37 @@
 class Solution {
 public:
     vector<vector<vector<long long>>> memo;
-    // (curr, prev, tight);
-    long long dp(string& s, int i, int prev, bool tight, int& k){
+    long long count(string& s, int i, int prev, bool tight, int& k){
         if(i==s.size()) return 1;
         if(memo[i][prev][tight]!=-1) return memo[i][prev][tight];
         long long ans = 0;
-        int lb = (prev==10) ? 0 : max(0, prev-k);
-        int ub = (prev==10) ? 9 : min(9, prev+k);
-        if(tight) ub = min(s[i]-'0', ub);
-
-        for(int j=lb; j<=ub; j++){
-            int n_prev = (prev==10 && j==0) ? 10 : j;
-            ans += dp(s, i+1, n_prev, (tight && j==(s[i]-'0')), k);
+        int maxi;
+        int mini;
+        if(prev==10){
+            maxi = 9;
+            mini = 0;
+        }
+        else{
+            maxi = min(9, prev + k);
+            mini = max(0, prev - k);
+        }
+        if(tight) maxi = min(s[i]-'0', maxi);
+        // cout<<prev<<" "<<mini<<" "<<maxi<<endl;
+        for(int t=mini; t<=maxi; t++){
+            int p = (t==0 && prev==10) ? 10 : t;
+            ans += count(s, i+1, p, (tight && (t==s[i]-'0')), k);
         }
         return memo[i][prev][tight] = ans;
-    }
-    long long terms(string& s, int& k){
-        memo.assign(s.size(), vector<vector<long long>>(11, vector<long long>(2, -1)));
-        return dp(s, 0, 10, true, k);
+
     }
     long long goodIntegers(long long l, long long r, int k) {
-        string s = to_string(r);
-        long long right = terms(s, k);
-        s = to_string(l-1);
-        long long left = terms(s, k);
-        return right - left;       
-
+      string one = to_string(l-1);
+      string two = to_string(r);
+      memo.assign(one.size(), vector<vector<long long>>(11, vector<long long>(2, -1)));
+      long long first = count(one, 0, 10, true, k);
+      memo.assign(two.size(), vector<vector<long long>>(11, vector<long long>(2, -1)));
+      long long second = count(two, 0, 10, true, k);
+    //   cout<<first<<" "<<second;
+      return second - first;
     }
 };
