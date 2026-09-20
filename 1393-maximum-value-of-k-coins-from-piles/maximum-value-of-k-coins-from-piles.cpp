7219543ebd8e4f8a2533a971bc4c663d97908532
kharkwal010 <kharkwalankit010@gmail.com>
2026-09-20 12:16:07 +0530

    Time: 127 ms (79.25%) | Memory: 34.3 MB (52.25%) - LeetSync

--- 1393-maximum-value-of-k-coins-from-piles/maximum-value-of-k-coins-from-piles.cpp
+++ 1393-maximum-value-of-k-coins-from-piles/maximum-value-of-k-coins-from-piles.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>> memo;
-    int coins(vector<vector<int>>& piles, int k, int i){
-        if(k==0 || i==piles.size()) return 0;
-        if(memo[i][k]!=-1) return memo[i][k];
+   vector<vector<int>> memo;
+   int coins(vector<vector<int>>& piles, int k, int i){
+        if(i==piles.size() || k==0) return 0;
+        if(memo[i][k] !=-1) return memo[i][k];
         int ans = 0;
         ans = max(ans, coins(piles, k, i+1));
-        int last = min((int)piles[i].size(), k);
+        int coin = 0;
         int sum = 0;
-        for(int j=0; j<last; j++){
-            sum += piles[i][j];
-            ans = max(ans, sum + coins(piles, k-j-1, i+1));
+        for(int ele: piles[i]){
+            if(coin==k) break;
+            coin++;
+            sum += ele;
+            ans = max(ans, sum + coins(piles, k-coin, i+1));            
         }
         return memo[i][k] = ans;
-    }
+
+
+   }
     int maxValueOfCoins(vector<vector<int>>& piles, int k) {
         int n = piles.size();
-        memo.resize(n, vector<int>(k+1, -1));
+        memo.assign(n, vector<int>(k+1, -1));
         return coins(piles, k, 0);
     }
 };
