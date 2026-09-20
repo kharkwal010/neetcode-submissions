a4745fe60f704d93a0d0cae170cbb675d4d437db
kharkwal010 <kharkwalankit010@gmail.com>
2026-09-20 15:37:50 +0530

    Time: 236 ms (23.46%) | Memory: 136.9 MB (18.99%) - LeetSync

--- 808-number-of-matching-subsequences/number-of-matching-subsequences.cpp
+++ 808-number-of-matching-subsequences/number-of-matching-subsequences.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     int numMatchingSubseq(string s, vector<string>& words) {
-        vector<queue<pair<string, int>>> buckets(26);
-        for(string& w: words){
-            buckets[w[0]-'a'].push({w, 0});
-        }
-        int ans = 0;
-        for(char& c: s){
-            int r = c-'a';
-            queue<pair<string, int>> temp;
-            while(!buckets[r].empty()){
-                auto[st, index] = buckets[r].front();
-                buckets[r].pop();
-                if(index+1==st.size()) ans++;
-                else{
-                    int nr = st[index+1]-'a';
-                    if(r==nr) temp.push({st, index+1});
-                    else buckets[nr].push({st, index+1});
-                }
+       int count = 0;
+       vector<vector<string>> terms(26);
+       for(string w: words){
+        terms[w[w.size()-1]-'a'].push_back(w);
+       }
+       for(int i=s.size()-1; i>=0; i--){
+        int curr = s[i]-'a';
+        vector<string> temp;
+        for(auto& ele: terms[curr]){
+            if(ele.size()==1){
+                count++;
+                continue;
             }
-            buckets[r] = temp;
+            ele.pop_back();
+            int newi = ele[ele.size()-1]-'a';
+            if(newi==curr) temp.push_back(ele);
+            else terms[newi].push_back(ele);
         }
-        return ans;
+        terms[curr] = temp;
+       }
+       return count;
+
     }
 };
