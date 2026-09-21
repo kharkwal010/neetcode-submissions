b111396879b01a1d406874038826c3c63c71b408
kharkwal010 <kharkwalankit010@gmail.com>
2026-09-21 12:25:59 +0530

    Time: 3 ms (55.71%) | Memory: 16.9 MB (57.86%) - LeetSync

--- 945-snakes-and-ladders/snakes-and-ladders.cpp
+++ 945-snakes-and-ladders/snakes-and-ladders.cpp
@@ -1,38 +1,35 @@
 class Solution {
 public:    
     int snakesAndLadders(vector<vector<int>>& board) {
-        int n = board.size();
-        vector<bool> visited(n*n+1, false);
-        queue<int> q;
-        q.push(1);
-        int count = 0;
-        visited[1]=true;
-        while(!q.empty()){
+       int n = board.size();
+       vector<int> visited(n*n+1, false);
+       queue<int> q;
+       q.push(1);
+       visited[1] = true;
+       int count = 1;
+       while(!q.empty()){
             int sz = q.size();
-            // cout<<sz<<endl;
             for(int i=0; i<sz; i++){
                 int curr = q.front();
-                // cout<<curr<<endl;
-                if(curr==n*n) return count;
                 q.pop();
-                for(int i=1; i<=6; i++){
-                    int newi = curr + i;
-                    if(newi>n*n) break;
-                    int r = n-1 - (newi-1)/n;
-                    int c = (newi-1)%n;
-                    if(((newi-1)/n)%2==1) c = n-1 - c;
-                    if(board[r][c]!=-1){
-                        newi = board[r][c];
-                    }
-                    if(visited[newi]) continue;
-                    visited[newi] = true;
-                    q.push(newi);
+                for(int j=1; j<=6; j++){
+                    int m = curr + j;
+                    int x = (m-1)/n;
+                    int r = n-1 - x;
+                    int c = (m-1)%n;
+                    if(x%2==1) c = n-1 - c;
+                    // cout<<r<<" "<<c<<endl;
+                    if(board[r][c]!=-1) m = board[r][c];
+                    if(visited[m]) continue;
+                    if(m==n*n) return count;
+                    visited[m] = true;
+                    q.push(m);
+
                 }
             }
-            // cout<<endl;
             count++;
-            
-        }
-        return -1;
+       }
+       return -1;
+       
     }
 };
