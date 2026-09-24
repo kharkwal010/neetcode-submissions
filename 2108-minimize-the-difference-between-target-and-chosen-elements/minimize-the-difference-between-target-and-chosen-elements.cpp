6d3842ed6c96d86b04b24d49f68122d048a371e1
kharkwal010 <kharkwalankit010@gmail.com>
2026-09-24 18:07:45 +0530

    Time: 334 ms (86.73%) | Memory: 25.6 MB (55.66%) - LeetSync

---
+++ 2108-minimize-the-difference-between-target-and-chosen-elements/minimize-the-difference-between-target-and-chosen-elements.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> memo;
+    vector<int> negsum;
+    int value(vector<vector<int>>& mat, int target, int i){
+        if(i==mat.size()) return abs(target);
+        if(target<=negsum[i]){
+            return negsum[i] - target;
+        }
+        if(memo[target][i]!=-1) return memo[target][i];
+        int ans = INT_MAX;
+        for(int j=0; j<mat[i].size(); j++){
+            ans = min(ans, value(mat, target-mat[i][j], i+1));
+        }
+        return memo[target][i] = ans;
+    }
+
+    int minimizeTheDifference(vector<vector<int>>& mat, int target) {
+        int r = mat.size();
+        int c = mat[0].size();
+        memo.resize(target+1, vector<int>(r, -1));
+        negsum.resize(r);
+        for(int i=0; i<r; i++){
+            negsum[i] = *min_element(mat[i].begin(), mat[i].end());
+        }
+        for(int i=r-2; i>=0; i--){
+            negsum[i]+=negsum[i+1];
+        }
+        negsum.push_back(0);
+        return value(mat, target, 0);
+        
+    }
+};
