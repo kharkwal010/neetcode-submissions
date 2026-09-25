200cae630821620656929f0ed212638cc779145f
kharkwal010 <kharkwalankit010@gmail.com>
2026-09-25 15:00:51 +0530

    Time: 7 ms (90.61%) | Memory: 14.2 MB (52.75%) - LeetSync

---
+++ 869-similar-string-groups/similar-string-groups.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool check(string& s, string& t){
+        int count = 0;
+        for(int i=0; i<s.size(); i++){
+            if(s[i]!=t[i]){
+                count++;
+                if(count>2) return false;
+            }
+        }
+        return true;
+    }
+    vector<int> parent;
+    int find(int a){
+        if(parent[a]==a) return a;
+        return parent[a] = find(parent[a]);
+    }
+
+    bool unite(int x, int y){
+        int px = find(x);
+        int py = find(y);
+        if(px==py) return false;
+        parent[px]= py;
+        return true;
+    }
+    int numSimilarGroups(vector<string>& strs) {
+        int n = strs.size();
+        parent.resize(n);
+        for(int i=0; i<n; i++) parent[i] = i;
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                if(parent[i]==parent[j]) continue;
+                if(check(strs[i], strs[j])) unite(i, j);
+            }
+        }
+        unordered_set<int> groups;
+        for(int i=0; i<n; i++){
+            groups.insert(find(i));
+        }
+        return (int)groups.size();
+
+    }
+};
